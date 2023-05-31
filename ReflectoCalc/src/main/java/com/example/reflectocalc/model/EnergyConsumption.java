f6d549bee45c8a282961ea0d570f313d72038117
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-05-31 21:26:45 +0300

    attributes

--- ReflectoCalc/src/main/java/com/example/reflectocalc/model/EnergyConsumption.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/model/EnergyConsumption.java
@@ -1,4 +1,19 @@
 package com.example.reflectocalc.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Entity
+@Table(name = "energy_consumptions")
 public class EnergyConsumption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "building_id", nullable = false)
+    private Building building;
+
+    @Column(nullable = false)
+    private double consumption;
 }
