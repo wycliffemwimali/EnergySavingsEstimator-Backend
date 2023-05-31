420039c45bfabdedd2b0711b8503fcfce0dd6aa1
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-05-31 20:14:20 +0300

    created springboot project

---
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/ReflectoCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.reflectocalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectoCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflectoCalcApplication.class, args);
+    }
+
+}
---
+++ ReflectoCalc/src/test/java/com/example/reflectocalc/ReflectoCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reflectocalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectoCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
