53a0b833392029418a4fc023a18b792491a949ea
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-05-31 21:20:10 +0300

    setters and getters

--- ReflectoCalc/src/main/java/com/example/reflectocalc/model/Roof.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/model/Roof.java
@@ -20,5 +20,35 @@ public class Roof {
     @JoinColumn(name = "building_id")
     private Building building;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
 }
