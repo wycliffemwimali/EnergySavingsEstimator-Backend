dd0d68081d89607789f86048c6e20017bbfd3803
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-05-31 21:19:30 +0300

    added attributes

--- ReflectoCalc/src/main/java/com/example/reflectocalc/model/Roof.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/model/Roof.java
@@ -1,20 +1,24 @@
 package com.example.reflectocalc.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Entity
 @Table(name = "roof")
 public class Roof {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "material")
+    private String material;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "area")
+    private double area;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "building_id")
+    private Building building;
+
+
 }
