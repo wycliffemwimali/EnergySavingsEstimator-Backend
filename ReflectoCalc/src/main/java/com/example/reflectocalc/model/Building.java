3b1fb963e2e4eb50a1d6b6ec18e1b2bf1358de00
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-05-31 21:15:35 +0300

    getters and setters

--- ReflectoCalc/src/main/java/com/example/reflectocalc/model/Building.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/model/Building.java
@@ -17,4 +17,28 @@ public class Building {
 
     @Column(name = "address")
     private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
