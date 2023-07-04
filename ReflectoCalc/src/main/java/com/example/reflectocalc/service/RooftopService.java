9bb4a36de41e1d12e9b0a18e143fb17de7ea8059
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-07-04 11:39:30 +0300

    rooftop serviceimpl

--- ReflectoCalc/src/main/java/com/example/reflectocalc/service/RooftopService.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/service/RooftopService.java
@@ -1,9 +1,14 @@
 package com.example.reflectocalc.service;
 
 import com.example.reflectocalc.model.Rooftop;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 
 public interface RooftopService {
     List<Rooftop> getRooftopsByBuildingId(Long buildingId);
+
+    void saveRooftop(Rooftop rooftop);
+
+    void deleteRooftopById(Long rooftopId) throws ChangeSetPersister.NotFoundException;
 }
--- ReflectoCalc/src/main/java/com/example/reflectocalc/service/RooftopServiceImpl.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/service/RooftopServiceImpl.java
@@ -1,7 +1,46 @@
 package com.example.reflectocalc.service;
 
+import com.example.reflectocalc.Repository.RooftopRepository;
+import com.example.reflectocalc.model.Rooftop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class RooftopServiceImpl {
+public class RooftopServiceImpl implements RooftopService {
+    private final RooftopRepository rooftopRepository;
+
+    @Autowired
+    public RooftopServiceImpl(RooftopRepository rooftopRepository) {
+        this.rooftopRepository = rooftopRepository;
+    }
+
+    @Override
+    public List<Rooftop> getRooftopsByBuildingId(Long buildingId) {
+        return rooftopRepository.findByBuildingId(buildingId);
+    }
+
+//    @Override
+//    public Rooftop getRooftopById(Long rooftopId) {
+//        return rooftopRepository.findById(rooftopId)
+//                .orElseThrow(() -> new NotFoundException("Rooftop not found with ID: " + rooftopId));
+//    }
+
+    @Override
+    public void saveRooftop(Rooftop rooftop) {
+        rooftopRepository.save(rooftop);
+    }
+
+    @Override
+    public void deleteRooftopById(Long rooftopId) throws ChangeSetPersister.NotFoundException {
+        if (!rooftopRepository.existsById(rooftopId)) {
+            throw new ChangeSetPersister.NotFoundException();
+        }
+        rooftopRepository.deleteById(rooftopId);
+    }
+
+    // Add more methods as needed
 }
+
