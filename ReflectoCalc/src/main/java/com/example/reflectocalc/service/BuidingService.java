1abca1fc6cb9fba3472d4c121dd51373356d2288
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-07-04 10:38:04 +0300

    contract for building service

---
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/service/BuidingService.java
@@ -0,0 +1,7 @@
+package com.example.reflectocalc.service;
+
+import com.example.reflectocalc.model.Building;
+
+public interface BuidingService {
+    double calculateEnergySavingsByReflectance(Building building);
+}
