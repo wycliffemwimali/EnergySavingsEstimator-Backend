89fd3c8e11963d50b7329f493f76bcc58d69bfe8
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-07-04 10:03:47 +0300

    custom query methods

--- ReflectoCalc/src/main/java/com/example/reflectocalc/Repository/BuildingRepository.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/Repository/BuildingRepository.java
@@ -4,6 +4,13 @@ import com.example.reflectocalc.model.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
+    // Custom query method to find buildings by city
+    List<Building> findByCity(String city);
+
+    // Custom query method to find buildings by owner name
+    List<Building> findByOwnerName(String ownerName);
 }
--- ReflectoCalc/src/main/java/com/example/reflectocalc/Repository/RoofRepository.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/Repository/RoofRepository.java
@@ -4,6 +4,13 @@ import com.example.reflectocalc.model.Roof;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoofRepository extends JpaRepository<Roof, Long> {
+    // Custom query method to find rooftops by building ID
+    List<Roof> findByBuildingId(Long buildingId);
+
+    // Custom query method to find rooftops by reflectance
+    List<Roof> findByReflectanceGreaterThan(double reflectance);
 }
