7e3ee5d9219ec966c8078c2f5e9a8ae010dde019
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-06 01:24:01 +0300

    constant factor changed: energysavingsservice

--- ReflectoCalc/src/main/java/com/example/reflectocalc/service/EnergySavingsService.java
+++ ReflectoCalc/src/main/java/com/example/reflectocalc/service/EnergySavingsService.java
@@ -1,6 +1,6 @@
 package com.example.reflectocalc.service;
 
 public interface EnergySavingsService {
-    double CONSTANT_FACTOR = 0.5;
+    double CONSTANT_FACTOR = 0.05;
     double calculateEnergySavingsByRooftopId(Long rooftopId);
 }
