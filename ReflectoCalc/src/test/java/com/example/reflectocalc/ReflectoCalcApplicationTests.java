8eabf08dc10315a42f17a3f3bfd0590905ef167e
wycliffemwimali <wycliffemwimali2@gmail.com>
2024-04-18 14:19:34 +0300

    get energy savings tests

--- ReflectoCalc/src/test/java/com/example/reflectocalc/ReflectoCalcApplicationTests.java
+++ ReflectoCalc/src/test/java/com/example/reflectocalc/ReflectoCalcApplicationTests.java
@@ -11,8 +11,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,7 +30,7 @@ class ReflectoCalcApplicationTests {
     private EnergySavingsController energySavingsController;
 
     @BeforeEach
-    void setUp() {
+    void init() {
         MockitoAnnotations.openMocks(this);
     }
     @Autowired
